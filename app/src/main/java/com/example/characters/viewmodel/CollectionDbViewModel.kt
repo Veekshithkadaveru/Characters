18b0603c0589c62ad567737822ab2abf281b3198
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-13 19:23:14 +0530

    Create Collection Db Viewmodel

---
+++ app/src/main/java/com/example/characters/viewmodel/CollectionDbViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.characters.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.characters.db.CollectionDbRepo
+import com.example.characters.db.DbCharacter
+import com.example.characters.model.CharacterResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CollectionDbViewModel @Inject constructor(private val repo: CollectionDbRepo) : ViewModel() {
+    val currentCharacter = MutableStateFlow<DbCharacter?>(null)
+    val collection = MutableStateFlow<List<DbCharacter>>(listOf())
+
+    init {
+        getCollection()
+    }
+
+    private fun getCollection() {
+        viewModelScope.launch {
+            repo.getCharactersFromRepo().collect {
+                collection.value = it
+            }
+        }
+    }
+
+    fun setCurrentCharacterId(characterId: Int?) {
+        characterId?.let {
+            viewModelScope.launch {
+                repo.getCharacterFromRepo(it).collect {
+                    currentCharacter.value = it
+                }
+            }
+        }
+    }
+
+    fun addCharacter(character: CharacterResult) {
+        viewModelScope.launch {
+            repo.addCharacterFromRepo(DbCharacter.fromCharacter(character))
+        }
+    }
+
+    fun deleteCharacter(character: DbCharacter) {
+        viewModelScope.launch {
+            repo.deleteCharacterFromRepo(character)
+        }
+    }
+}
