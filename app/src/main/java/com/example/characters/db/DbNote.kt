06b1d70e7fe93b6d8d0f77c9368a4a02654c3995
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-15 19:08:24 +0530

    Create Note database

---
+++ app/src/main/java/com/example/characters/db/DbNote.kt
@@ -0,0 +1,24 @@
+package com.example.characters.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.characters.model.Note
+
+@Entity(tableName = Constants.NOTE_TABLE)
+data class DbNote(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val characterId: Int,
+    val title: String,
+    val text: String
+) {
+    companion object {
+        fun fromNote(note: Note) =
+            DbNote(
+                id = 0,
+                characterId = note.characterId,
+                title = note.title,
+                text = note.text
+            )
+    }
+}
